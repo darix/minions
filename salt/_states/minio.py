e96c37a44643f1f086ab85dd948d65dc654a9bf4
Marcus Rückert <darix@nordisch.org>
2025-01-30 16:35:34 +0100

    Creating buckets works

--- salt/_states/minio.py
+++ salt/_states/minio.py
@@ -3,14 +3,18 @@ import tempfile
 import os
 import logging
 import json
+import minio
 
 
 minion_client_binary   = "/usr/bin/minio-client"
 salt_minion_config_dir = "/etc/salt/minio-client/"
+salt_minion_config_file = f"{salt_minion_config_dir}/config.json"
+salt_minion_alias       = "salt"
 
 from subprocess import PIPE, Popen
 from salt.exceptions import SaltConfigurationError, SaltRenderError
 import salt.utils.json
+from urllib.parse import urlparse
 
 log = logging.getLogger(__name__)
 
@@ -60,9 +64,55 @@ def run_dmc_to_json(args):
     return_data={}
   else:
     # TODO: error handling
-    return_data=json.load(mc_data)
+    try:
+      return_data=json.loads(mc_data)
+    except (json.decoder.JSONDecodeError) as e:
+      message=f"Failed to parse output from {' '.join(cmd)}: {mc_data}"
+      log.error(message)
+      raise SaltRenderError(message)
 
   return mc_returncode, return_data
+
+def parse_json_string(data):
+    try:
+      log.error(f"class: {data.__class__()} data: {data}")
+      return_data=json.loads(data)
+      return return_data
+    except (json.decoder.JSONDecodeError) as e:
+      message=f"Failed to parse json string: {e}: {data}"
+      log.error(message)
+      raise SaltRenderError(message)
+
+def parse_json_file(filename):
+    try:
+      return_data=json.load(open(filename))
+      return return_data
+    except (json.decoder.JSONDecodeError) as e:
+      message=f"Failed to parse json string from filename: {e}"
+      log.error(message)
+      raise SaltRenderError(message)
+
+def minio_credentials():
+  return minio.credentials.MinioClientConfigProvider(salt_minion_config_file, alias=salt_minion_alias)
+
+def minio_config():
+  parsed_config = parse_json_file(salt_minion_config_file)
+  if not("aliases" in parsed_config and salt_minion_alias in parsed_config["aliases"] and "url" in parsed_config["aliases"][salt_minion_alias]):
+      messsage = f"Can not find alias {salt_minion_alias} in {salt_minion_config_file}"
+      log.error(message)
+      raise SaltConfigurationError(message)
+
+  url = parsed_config["aliases"][salt_minion_alias]["url"]
+  netloc = urlparse(url).netloc
+  return netloc, parsed_config
+
+def minio_client():
+  netloc, parsed_config = minio_config()
+  return minio.Minio(endpoint=netloc, credentials=minio_credentials())
+
+def minio_admin():
+  netloc, parsed_config = minio_config()
+  return minio.MinioAdmin(endpoint=netloc, credentials=minio_credentials())
 
 def site_config(name, config={}):
   # config_array = []
@@ -79,11 +129,32 @@ def site_config(name, config={}):
 
 def bucket(name, data={}):
   return_data = {'name': name, 'result': True, 'changes': {"muahah": "wooohoo"}, 'comment': "darix was here"}
+  mc = minio_client()
 
-  return_code, bucket_list = run_dmc_to_json(["ls", instance_name])
+  bucket_list = mc.list_buckets()
 
-  if not name in bucket_list:
-    return_data["changes"]= {"what": f"Bucket {name} missing"}
+  found=False
+
+  for bucket in bucket_list:
+    log.error(f"Checking {name} against {bucket.name}")
+    if bucket.name == name:
+      found=True
+      break
+
+  if found:
+    return_data["changes"]= {"what": f"Bucket {name} already there"}
+  else:
+    try:
+      if not("minio" in __pillar__ and "location" in __pillar__["minio"]):
+        raise SaltConfigurationError("Missing location in minio pillar")
+      location = __pillar__["minio"]["location"]
+      locking = False
+      if "locking" in data:
+        locking = data["locking"]
+      mc.make_bucket(bucket_name=name, location=location, object_lock=locking)
+      return_data["changes"]= {"what": f"Bucket {name} was missing and is now added"}
+    except (ValueError) as e:
+      raise e
 
   return return_data
 
