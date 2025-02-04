c999744f5966607debe38cc73b61af88b22f1c4a
Marcus Rückert <darix@nordisch.org>
2025-02-04 19:16:27 +0100

    Implemented remaining bits of the user handling

--- salt/_states/minio.py
+++ salt/_states/minio.py
@@ -4,15 +4,17 @@
 # TODO: implement proper result_data
 #
 
-import tempfile
-import os
-import logging
 import json
+import logging
+import os
+import random
+import string
+import tempfile
+
 import minio as pyminio
 
-
-minion_client_binary   = "/usr/bin/minio-client"
-salt_minion_config_dir = "/etc/salt/minio-client/"
+minion_client_binary    = "/usr/bin/minio-client"
+salt_minion_config_dir  = "/etc/salt/minio-client/"
 salt_minion_config_file = f"{salt_minion_config_dir}/config.json"
 salt_minion_alias       = "salt"
 
@@ -25,7 +27,7 @@ log = logging.getLogger(__name__)
 __cmdenv = os.environ.copy()
 
 __policy_templates = {
-"pgbackrest-append-only": """
+  "pgbackrest-append-only": """
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -80,7 +82,7 @@ __policy_templates = {
     ]
 }""",
 
-"pgbackrest-full-permissions": """
+  "pgbackrest-full-permissions": """
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -126,7 +128,7 @@ __policy_templates = {
         }
     ]
 }""",
-"restic-append-only": """
+  "restic-append-only": """
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -154,7 +156,7 @@ __policy_templates = {
         }
     ]
 }""",
-"restic-full-permissions": """
+  "restic-full-permissions": """
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -206,16 +208,17 @@ root@fortress /srv/cfgmgmt # /usr/bin/minio-client --config-dir /etc/salt/minio-
 0
 """
 
+
 def __run_dmc_to_json(args):
-  cmd=[minion_client_binary, "--config-dir", salt_minion_config_dir, "--json", "--no-color", "--disable-pager"]
+  cmd = [minion_client_binary, "--config-dir", salt_minion_config_dir, "--json", "--no-color", "--disable-pager"]
   cmd.extend(args)
   try:
-      proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=__cmdenv, encoding="utf-8")
-      mc_data, mc_error = proc.communicate()
-      mc_returncode = proc.returncode
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=__cmdenv, encoding="utf-8")
+    mc_data, mc_error = proc.communicate()
+    mc_returncode = proc.returncode
   except (OSError, UnicodeDecodeError) as e:
-      mc_data, mc_error = "", str(e)
-      mc_returncode = 1
+    mc_data, mc_error = "", str(e)
+    mc_returncode = 1
 
   # unlike in our gopass tool we can not just raise here. The real raise has to happen in the specific function
   # that called us and knows how to handle the specific error
@@ -223,58 +226,66 @@ def __run_dmc_to_json(args):
   #   raise SaltRenderError(f"Running {cmd} failed. Please check the log.")
 
   if not mc_data:
-    return_data={}
+    return_data = {}
   else:
     # TODO: error handling
     try:
-      return_data=json.loads(mc_data)
+      return_data = json.loads(mc_data)
     except (json.decoder.JSONDecodeError) as e:
-      message=f"Failed to parse output from {' '.join(cmd)}: {mc_data}"
+      message = f"Failed to parse output from {' '.join(cmd)}: {mc_data}"
       log.error(message)
       raise SaltRenderError(message)
 
   return mc_returncode, return_data
 
+
 def __parse_json_string(data):
-    try:
-      log.error(f"class: {data.__class__()} data: {data}")
-      return_data=json.loads(data)
-      return return_data
-    except (json.decoder.JSONDecodeError) as e:
-      message=f"Failed to parse json string: {e}: {data}"
-      log.error(message)
-      raise SaltRenderError(message)
+  try:
+    log.error(f"class: {data.__class__()} data: {data}")
+    return_data = json.loads(data)
+    return return_data
+  except (json.decoder.JSONDecodeError) as e:
+    message = f"Failed to parse json string: {e}: {data}"
+    log.error(message)
+    raise SaltRenderError(message)
+
 
 def __parse_json_file(filename):
-    try:
-      return_data=json.load(open(filename))
-      return return_data
-    except (json.decoder.JSONDecodeError) as e:
-      message=f"Failed to parse json string from filename: {e}"
-      log.error(message)
-      raise SaltRenderError(message)
+  try:
+    return_data = json.load(open(filename))
+    return return_data
+  except (json.decoder.JSONDecodeError) as e:
+    message = f"Failed to parse json string from filename: {e}"
+    log.error(message)
+    raise SaltRenderError(message)
+
 
 def __minio_credentials():
   return pyminio.credentials.MinioClientConfigProvider(salt_minion_config_file, alias=salt_minion_alias)
 
+
 def __minio_config():
   parsed_config = __parse_json_file(salt_minion_config_file)
-  if not("aliases" in parsed_config and salt_minion_alias in parsed_config["aliases"] and "url" in parsed_config["aliases"][salt_minion_alias]):
-      messsage = f"Can not find alias {salt_minion_alias} in {salt_minion_config_file}"
-      log.error(message)
-      raise SaltConfigurationError(message)
+  if not ("aliases" in parsed_config and salt_minion_alias in parsed_config["aliases"] and "url" in
+          parsed_config["aliases"][salt_minion_alias]):
+    messsage = f"Can not find alias {salt_minion_alias} in {salt_minion_config_file}"
+    log.error(message)
+    raise SaltConfigurationError(message)
 
   url = parsed_config["aliases"][salt_minion_alias]["url"]
   netloc = urlparse(url).netloc
   return netloc, parsed_config
 
+
 def __minio_client():
   netloc, parsed_config = __minio_config()
   return pyminio.Minio(endpoint=netloc, credentials=__minio_credentials())
 
+
 def __minio_admin():
   netloc, parsed_config = __minio_config()
   return pyminio.MinioAdmin(endpoint=netloc, credentials=__minio_credentials())
+
 
 def site_config(name, config={}):
   # config_array = []
@@ -289,43 +300,46 @@ def site_config(name, config={}):
   #   cmd [minion_client_binary]
   return {'name': name, 'result': True, 'changes': {}, 'comment': "darix was here"}
 
+
 def bucket_present(name, data={}):
   return_data = {'name': name, 'result': True, 'changes': {"muahah": "wooohoo"}, 'comment': "darix was here"}
 
   mc = __minio_client()
-  found=mc.bucket_exists(name)
+  found = mc.bucket_exists(name)
 
   if found:
     # TODO: implement update mode for settings
-    return_data["changes"]= {"what": f"Bucket {name} already there"}
+    return_data["changes"] = {"what": f"Bucket {name} already there"}
   else:
     try:
-      if not("minio" in __pillar__ and "location" in __pillar__["minio"]):
+      if not ("minio" in __pillar__ and "location" in __pillar__["minio"]):
         raise SaltConfigurationError("Missing location in minio pillar")
       location = __pillar__["minio"]["location"]
       locking = False
       if "locking" in data:
         locking = data["locking"]
       mc.make_bucket(bucket_name=name, location=location, object_lock=locking)
-      return_data["changes"]= {"what": f"Bucket {name} was missing and is now added"}
+      return_data["changes"] = {"what": f"Bucket {name} was missing and is now added"}
     except (ValueError) as e:
       raise e
 
   return return_data
 
+
 def bucket_missing(name):
   return_data = {'name': name, 'result': True, 'changes': {"muahah": "wooohoo"}, 'comment': "darix was here"}
 
   mc = __minio_client()
-  found=mc.bucket_exists(name)
+  found = mc.bucket_exists(name)
 
   if found:
     mc.remove_bucket(name)
-    return_data["changes"]= {"what": f"Bucket {name} removed"}
+    return_data["changes"] = {"what": f"Bucket {name} removed"}
   else:
-    return_data["changes"]= {"what": f"Bucket {name} already missing"}
+    return_data["changes"] = {"what": f"Bucket {name} already missing"}
 
   return return_data
+
 
 def policy_present(name, data={}):
   return_data = {'name': name, 'result': True, 'changes': {}, 'comment': "darix was here"}
@@ -335,16 +349,16 @@ def policy_present(name, data={}):
 
   if name in policy_list:
     # TODO: implement update mode
-    return_data["changes"]= {"what": f"Policy {name} already there"}
+    return_data["changes"] = {"what": f"Policy {name} already there"}
   else:
     if not data["type"] in __policy_templates:
       raise SaltConfigurationError(f'No template for policy type {data["type"]}')
-    template=__policy_templates[data["type"]]
+    template = __policy_templates[data["type"]]
     with tempfile.NamedTemporaryFile(mode="w+", encoding='utf-8', delete=False) as policy_file:
       try:
-        policy_string=template
-        for k,v in data.items():
-          policy_string=policy_string.replace(f"%%{k}%%", v)
+        policy_string = template
+        for k, v in data.items():
+          policy_string = policy_string.replace(f"%%{k}%%", v)
         policy_file.write(policy_string)
         policy_file.close()
         ma.policy_add(name, policy_file.name)
@@ -354,9 +368,10 @@ def policy_present(name, data={}):
       finally:
         os.remove(policy_file.name)
 
-    return_data["changes"]= {"what": f"Policy {name} is missing"}
+    return_data["changes"] = {"what": f"Policy {name} is missing"}
 
   return return_data
+
 
 def policy_missing(name):
   return_data = {'name': name, 'result': True, 'changes': {}, 'comment': "darix was here"}
@@ -366,57 +381,100 @@ def policy_missing(name):
 
   if name in policy_list:
     ma.policy_remove(name)
-    return_data["changes"]= {"what": f"Policy {name} removed"}
+    return_data["changes"] = {"what": f"Policy {name} removed"}
   else:
-    return_data["changes"]= {"what": f"Policy {name} is already missing"}
+    return_data["changes"] = {"what": f"Policy {name} is already missing"}
 
   return return_data
+
+
+def _gimme_random_string(length=20):
+  return (''.join(random.choices(string.ascii_letters + string.digits, k=length)))
+
+
+def _filter_existing_policies(current_policies_string, new_policies_list=[]):
+  current_policies_list = current_policies_string.split(',')
+  for policy in current_policies_list:
+    if policy in new_policies_list:
+      new_policies_list.remove(policy)
+  return new_policies_list
+
+
+def _get_existing_service_accounts(ma, name):
+  parsed_json = __parse_json_string(ma.list_service_account(name))
+  current_sacct_list = []
+  if "accounts" in parsed_json:
+    current_sacct_list = parsed_json["accounts"]
+    if current_sacct_list == None:
+      current_sacct_list = []
+  else:
+    raise SaltConfigurationError(f"No idea how to parse the list of current service accounts: {parsed_json}")
+  return current_sacct_list
+
+
+def _existing_service_account(current_sacct_list, account_data):
+  for current_sacct in current_sacct_list:
+    if current_sacct["accessKey"] == account_data["access_key"]:
+      return True
+  return False
+
+def _verify_service_account(return_data, account_data):
+  parsed_data = __parse_json_string(return_data)
+  return (parsed_data["credentials"]["accessKey"] == account_data["access_key"] and parsed_data["credentials"]["secretKey"] == account_data["secret_key"])
 
 def user_present(name, data={}):
   return_data = {'name': name, 'result': True, 'changes': {}, 'comment': "darix was here"}
 
   ma = __minio_admin()
-  user_list = ma.user_list()
+  user_list = __parse_json_string(ma.user_list())
 
   if name in user_list:
     # TODO: implement update mode
-    return_data["changes"]= {"what": f"User '{name}' already there"}
+    return_data["changes"]["what"] = f"User '{name}' already there"
+    user_data = user_list[name]
   else:
-    if not("password" in data):
+    user_data = {}
+    if not ("password" in data):
       raise SaltConfigurationError(f"missing password for user '{name}'")
-    ma.user_add(name, data["password"])
-    if "policies" in data:
-      ma.attach_policy(policies=data["policies"], user=name)
-      if "service_accounts" in data:
-         for account_name, account_data in data["service_accounts"].items():
-           if not("access_key" in account_data and "secret_key" in account_data):
-             raise SaltConfigurationError(f"missing data for service account '{account_name}' for user '{name}' ")
-           # TODO:
-           # this does not actually set up a service account per user:
-           # https://github.com/minio/minio-py/issues/1481
-           # mc --disable-pager admin user svcacct list local mimir
-           # mc --disable-pager admin user svcacct add local mimir --access-key=fooooofooooo --secret-key=baaaarbaaaar --name foobar
-           #    --access-key value            set an access key for the service account
-           #    --secret-key value            set a secret key for the service account
-           #    --policy value                path to a JSON policy file
-           #    --name value                  friendly name for the service account
-           #    --description value           description for the service account
-           #    --expiry value                time of expiration for the service account
-           # TODO: should we have autorotation?
-           #
-           ma.add_service_account(access_key=account_data["access_key"], secret_key=account_data["secret_key"], name=account_name)
+    if ma.user_add(name, data["password"]):
+      return_data["changes"]["user"] = f"User '{name}' already created"
+  if "policies" in data:
+    new_policies = data["policies"]
+    if "policyName" in user_data:
+      new_policies = _filter_existing_policies(user_data["policyName"], new_policies)
+    if len(new_policies) > 0:
+      if ma.attach_policy(policies=new_policies, user=name):
+        return_data["changes"]["policies"] = f"Policies {','.join(data['policies'])} attached to user '{name}'"
+    else:
+      return_data["changes"]["policies"] = f"All Policies were already attached to user '{name}'"
+  if "service_accounts" in data:
+    current_sacct_list = _get_existing_service_accounts(ma, name)
+    for account_name, account_data in data["service_accounts"].items():
+      if not ("access_key" in account_data and "secret_key" in account_data):
+        raise SaltConfigurationError(f"missing data for service account '{account_name}' for user '{name}' ")
+      if _existing_service_account(current_sacct_list, account_data):
+        return_data["changes"][
+          f"svsacct_{account_name}"] = f"Service account {account_name}  with {account_data['access_key']} for user {name} already exists"
+      else:
+        rd = ma.add_service_account(access_key=account_data['access_key'], secret_key=account_data['secret_key'], name=account_name, targetUser=name)
+        log.error(f"add service account rd {rd}")
+        if _verify_service_account(rd, account_data):
+          return_data["changes"][f"svsacct_{account_name}"] = f"Created service account {account_name} for user {name}"
+        else:
+          raise SaltConfigurationError(f"Something went wrong while configuring service account {account_name} for user {name}")
 
   return return_data
+
 
 def user_missing(name):
   return_data = {'name': name, 'result': True, 'changes': {}, 'comment': "darix was here"}
   ma = __minio_admin()
   try:
     ma.user_remove(name)
-    return_data["changes"]= {"what": f"User '{name}' removed"}
+    return_data["changes"] = {"what": f"User '{name}' removed"}
   except (pyminio.error.MinioAdminException) as e:
     if e._code == '404':
-        return_data["changes"]= {"what": f"User '{name}' already missing"}
+      return_data["changes"] = {"what": f"User '{name}' already missing"}
     else:
       raise e
 
